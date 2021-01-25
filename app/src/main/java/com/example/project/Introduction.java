710f216f1c95407d63eab25857a56b12f9b6dfce
U180709008 <ahmetoral@posta.mu.edu.tr>
2021-01-25 14:47:23 +0300

    commit

--- app/src/main/java/com/example/project/Introduction.java
+++ app/src/main/java/com/example/project/Introduction.java
@@ -28,17 +28,28 @@ public class Introduction extends AppCompatActivity {
 
     private FirebaseAuth auth;
 
-    private EditText password, phone, firstname, lastname,email;
+
+
+    private EditText password, phone, firstname, lastname, email, usertelephone;
     ProgressDialog logup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_introduction);
+        init();
+        init2();
+    }
 
 
     public void  init () {
 
-        email = findViewById(R.id.EmailCreate);
-        password = findViewById(R.id.Password);
-        phone = findViewById(R.id.Telephone);
-        firstname = findViewById(R.id.FirstName);
-        lastname = findViewById(R.id.LastName);
+        email = (EditText) findViewById(R.id.EmailCreate);
+        password = (EditText) findViewById(R.id.Password);
+        phone = (EditText) findViewById(R.id.Telephone);
+        firstname = (EditText) findViewById(R.id.FirstName);
+        lastname = (EditText) findViewById(R.id.LastName);
+        usertelephone = (EditText) findViewById(R.id.Telephone);
 
 
         auth = FirebaseAuth.getInstance();
@@ -46,49 +57,67 @@ public class Introduction extends AppCompatActivity {
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                addValuesToDatabase();
                 createNewAccount();
             }
         });
         logup = new ProgressDialog(this);
     }
 
-            private void createNewAccount() {
-                String txtEmail = email.getText().toString();
-                String txtPassword = password.getText().toString();
+    private void addValuesToDatabase(){
+        rootNode = FirebaseDatabase.getInstance();
+        reference = rootNode.getReference("users");
+
+        //get all the values
+        String firstName = firstname.getText().toString();
+        String lastName = lastname.getText().toString();
+        String txtEmail = email.getText().toString();
+        String telephone = phone.getText().toString();
+        String txtPassword = password.getText().toString();
+
+        UserHelperClass helperClass = new UserHelperClass(firstName, lastName, txtEmail, telephone, txtPassword);
+
+        reference.child(firstName).setValue(helperClass);
+    }
+
+    private void createNewAccount() {
+        String txtEmail = email.getText().toString();
+        String txtPassword = password.getText().toString();
 
 
-                if (TextUtils.isEmpty(txtEmail)) {
-                    Toast.makeText(this, "E-MAIL FIELD CANNOT BE EMPTY!!!", Toast.LENGTH_LONG).show();
-                } else if (TextUtils.isEmpty(txtPassword)) {
-                    Toast.makeText(this, "PASSWORD FIELD CANNOT BE EMPTY!!!", Toast.LENGTH_LONG).show();
-                } else {
-                    //progress
-                    logup.setTitle("Signin Up...");
-                    logup.setMessage("Please Wait...");
-                    logup.setCanceledOnTouchOutside(true);
-                    logup.show();
+        if (TextUtils.isEmpty(txtEmail)) {
+            Toast.makeText(this, "E-MAIL FIELD CANNOT BE EMPTY!!!", Toast.LENGTH_LONG).show();
+        } else if (TextUtils.isEmpty(txtPassword)) {
+            Toast.makeText(this, "PASSWORD FIELD CANNOT BE EMPTY!!!", Toast.LENGTH_LONG).show();
+        } else {
+            //progress
+            logup.setTitle("Signin Up...");
+            logup.setMessage("Please Wait...");
+            logup.setCanceledOnTouchOutside(true);
+            logup.show();
 
 
 
-                    auth.createUserWithEmailAndPassword(txtEmail, txtPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                         if(task.isSuccessful()) {
-                             Toast.makeText(Introduction.this, "REGISTRATION SUCCESSFUL!!!\n", Toast.LENGTH_LONG).show();
-                             Intent intent = new Intent(Introduction.this, SignIn.class);
-                             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                             startActivity(intent);
-                             logup.dismiss();
-                             finish();
-                         }else {
-                             String message = task.getException().toString();
-                             Toast.makeText(Introduction.this, "REGISTRATION WRONG!!!\n" + "Errors: " + message, Toast.LENGTH_LONG).show();
-                             logup.dismiss();
-                         }
-                        }
-                    });
+            auth.createUserWithEmailAndPassword(txtEmail, txtPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()) {
+                        Toast.makeText(Introduction.this, "REGISTRATION SUCCESSFUL!!!\n", Toast.LENGTH_LONG).show();
+                        Intent intent = new Intent(Introduction.this, SignIn.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+                        logup.dismiss();
+                        finish();
+                    }else {
+                        String message = task.getException().toString();
+                        Toast.makeText(Introduction.this, "REGISTRATION WRONG!!!\n" + "Errors: " + message, Toast.LENGTH_LONG).show();
+                        logup.dismiss();
+                    }
                 }
-            }
+            });
+        }
+    }
+
 
 
     public void init2(){
@@ -101,14 +130,8 @@ public class Introduction extends AppCompatActivity {
                    startActivity(intent);
                }
            });
-       }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_introduction);
-        init();
-        init2();
-    }}
 
 
+
+
+    } }
