9670b2fa8ad74253078853664a9ae6a8908ffc49
U180709008 <ahmetoral@posta.mu.edu.tr>
2021-01-26 04:58:36 +0300

    commit

--- app/src/main/java/com/example/project/Customer_MyPlaces.java
+++ app/src/main/java/com/example/project/Customer_MyPlaces.java
@@ -1,18 +1,30 @@
 package com.example.project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Customer_MyPlaces extends AppCompatActivity {
     private Toolbar toolbar;
+    Button btn1;
 
     public void init(){
         toolbar = findViewById(R.id.action_barmyPlaces);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("My Places");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        btn1 = findViewById(R.id.mysaloons1);
     }
 
     @Override
@@ -20,5 +32,31 @@ public class Customer_MyPlaces extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_places);
         init();
+
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Saloon/OnurSaloon/Name");
+        myRef.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String name = dataSnapshot.getValue(String.class);
+                btn1.setText(name);
+
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Customer_MyPlaces.this, onursSaloon.class);
+                startActivity(intent);
+
+            }
+        });
+
+
     }
 }
